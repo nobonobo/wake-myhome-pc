7137c77ba43b11f949e9c02114b3a98831fd1c58
NoboNobo <irieda@gmail.com>
2024-04-09 16:46:57 +0900

    addd button control

--- main.go
+++ main.go
@@ -22,20 +22,20 @@ type Client struct {
 	closer []func() error
 }
 
-func NewClient(keypair crypt.KeyPair, peerPublicKey crypt.Key) *Client {
+func NewClient(keypair crypt.KeyPair, peerPublicKey crypt.Key) (*Client, error) {
 	c := &Client{}
 	pc, err := peer.Open(keypair, peerPublicKey)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	c.closer = append(c.closer, pc.Close)
 	conn, err := pc.Open(8080)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	c.closer = append(c.closer, conn.Close)
 	c.Client = jsonrpc.NewClient(conn)
-	return c
+	return c, nil
 }
 
 func (c *Client) Close() error {
@@ -45,36 +45,58 @@ func (c *Client) Close() error {
 	return nil
 }
 
+var (
+	window   = js.Global().Get("window")
+	document = window.Get("document")
+)
+
 func main() {
 	fp, err := secret.Open("secret.env")
 	if err != nil {
 		log.Print(err)
+		window.Call("alert", err.Error())
+		return
 	}
 	d, err := godotenv.Parse(fp)
 	if err != nil {
 		log.Print(err)
+		window.Call("alert", err.Error())
+		return
 	}
 	signal.SetDefaultOptions(
 		signal.WithChannel(channels.Must(channels.Get("operator://operator.irieda.com"))),
 	)
 	pub, err := crypt.NewKey(d["PUBLIC_KEY"])
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		window.Call("alert", err.Error())
+		return
 	}
 	priv, err := crypt.NewKey(d["PRIVATE_KEY"])
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		window.Call("alert", err.Error())
+		return
 	}
 	keyPair := crypt.KeyPair{Public: pub, Private: priv}
 	peerPub, err := crypt.NewKey(d["PEER_PUBLIC_KEY"])
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		window.Call("alert", err.Error())
+		return
 	}
-	client := NewClient(keyPair, peerPub)
+	client, err := NewClient(keyPair, peerPub)
+	if err != nil {
+		log.Print(err)
+		window.Call("alert", err.Error())
+		return
+	}
 	js.Global().Set("WakeOnLan", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		go client.Call("Node.WakeOnLan", "08:BF:B8:8D:32:43", nil)
 		return nil
 	}))
 	defer client.Close()
+	btn := document.Call("getElementById", "wake-btn")
+	btn.Set("disabled", false)
 	select {}
 }
